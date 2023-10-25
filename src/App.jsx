4dc6186b014b4fdd7c46ad4063102cf80741d5e9
hamiz <reshma9298h@gmail.com>
2023-10-25 12:18:19 +0530

    Implement UseContext

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import CartPage from './components/CartPage';
+import { products } from './components/productsData';
+
+const CartContext = React.createContext();
+
+function App() {
+  return (
+    <Container>
+      <CartContext.Provider value={products}>
+        <CartPage />
+      </CartContext.Provider>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/productsData.jsx
@@ -0,0 +1,11 @@
+// components/productsData.jsx
+export const products = [
+  {
+    id: 1,
+    title: 'iPhone 9',
+    description: 'An apple mobile which is nothing like apple',
+    price: 549,
+    stock: 94,
+    images: ['https://i.dummyjson.com/data/products/1/1.jpg'],
+  },
+];
